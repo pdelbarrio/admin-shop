965031c548d63a885fe19e6d7a41a716e4c6d8b1
Pablo del Barrio <pablodbb@gmail.com>
2024-12-02 12:45:15 +0100

    configuration of authstore with Pinia

---
+++ src/modules/auth/stores/auth.store.ts
@@ -0,0 +1,51 @@
+import { ref, computed } from 'vue';
+import { defineStore } from 'pinia';
+import { AuthStatus, type User } from '../interfaces';
+import { loginAction } from '../actions';
+
+export const useAuthStore = defineStore('auth', () => {
+  const authStatus = ref<AuthStatus>(AuthStatus.Checking);
+  const user = ref<User | undefined>();
+  const token = ref('');
+
+  const login = async (email: string, password: string) => {
+    try {
+      const loginResp = await loginAction(email, password);
+      if (!loginResp.ok) {
+        return false;
+      }
+
+      user.value = loginResp.user;
+      token.value = loginResp.token;
+      authStatus.value = AuthStatus.Authenticated;
+
+      return true;
+    } catch (error) {
+      return logout;
+    }
+  };
+
+  const logout = () => {
+    authStatus.value = AuthStatus.Unauthenticated;
+    user.value = undefined;
+    token.value = '';
+    return false;
+  };
+
+  return {
+    user,
+    token,
+    authStatus,
+
+    //Getters
+    isChecking: computed(() => authStatus.value === AuthStatus.Checking),
+    isAuthenticated: computed(() => authStatus.value === AuthStatus.Authenticated),
+
+    //Todo: getter para saber si es Admin
+
+    username: computed(() => user.value?.fullName),
+
+    //Actions
+    login,
+  };
+});
